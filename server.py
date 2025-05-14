e70bf7690835c8e6d368eac001cb7a0dbbba7868
renben-dev <bidone.renzo@gmail.com>
2025-05-14 13:09:54 -0400

    rolling update

--- server.py
+++ server.py
@@ -1,3 +1,8 @@
+""" Executing this function initiates the application of EMOTION
+    DETECTION to be executed over the Flask channel and deployed on
+    localhost:5000.
+"""
+
 from flask import Flask, request, render_template
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,15 +10,24 @@ app = Flask(__name__)
 
 @app.route('/emotionDetector')
 def emo_detector():
+    ''' This FUNTION receives the text from the HTML interface and 
+        runs EMOTION DETECTION over it using emotion_detector() function.
+        The output returned shows the emotionsl and the dominant emotion 
+        for the provided text.
+    '''
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
     try:
         status_code = response['status_code']
         if response['dominant_emotion'] is None:
             return "Invalid text! Please try again!"
-    except:
-        status_code = 500
-    
+    except ValueError as e:
+        print(f"ValueError occurred: {e}")
+    except TypeError as e:
+        print(f"TypeError occurred: {e}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
     if response and status_code == 200:
         try:
             response.pop('status_code', None)
@@ -27,9 +41,10 @@ def emo_detector():
         sadness = response['sadness']
         dom = response['dominant_emotion']
 
-        return f"'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy}, 'sadness': {sadness}. The dominant emotion is <b>{dom}</b>."
-    else:        
-        return f"The text provided is invalid. Status code = {status_code}"
+        return (f"'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, "
+                f"'joy': {joy}, 'sadness': {sadness}. The dominant emotion is <b>{dom}</b>."
+        )
+    return f"The text provided is invalid. Status code = {status_code}"
 
 @app.route("/")
 def render_index_page():
